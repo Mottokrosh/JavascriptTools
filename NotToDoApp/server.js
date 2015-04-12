00d8f11e613c551c9b610bce9733b739ab75f218
Frank Reding <frank.reding@gmail.com>
2015-04-12 11:48:22 +0100

    First draft

--- NotToDoApp/server.js
+++ NotToDoApp/server.js
@@ -1,6 +1,9 @@
 // Load the http module to create an http server.
 var http = require('http');
 var argv = require('yargs').argv;
+var tips = require('./dump.json');
+
+console.log(tips.general);
 
 // Configure our HTTP server to respond with Hello World to all requests.
 var server = http.createServer(function (request, response) {
